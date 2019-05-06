1e81de24d0b16df29c8014c0f32ecac0e0a69323
web.guru@me.com <web.guru@me.com>
2019-05-07 02:47:07 +0530

    update gemfile and config

--- config.rb
+++ config.rb
@@ -1,27 +1,31 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+require "uglifier"
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
+# Assests pipeline
+activate :sprockets
 
 # Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page "/partials/*", layout: false
+page "/admin/*", layout: false
 
-# Assests pipeline
-activate :sprockets 
+helpers do
+  def markdown(content)
+    Tilt['markdown'].new(context: @app) { content }.render
+  end
+end
 
 configure :build do
   # Minify css on build
   activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript, :ignore => "**/admin/**", compressor: ::Uglifier.new(mangle: true, compress: { drop_console: true }, output: {comments: :none})
+  activate :minify_javascript, :ignore => "**/admin/**", compressor: ::Uglifier.new(mangle: true, compress: { drop_console: true }, output: {comments: :none})
 
   # Use Gzip
   activate :gzip
@@ -29,4 +33,13 @@ configure :build do
   #Use asset hashes to use for caching
   #activate :asset_hash
 
+end
+
+configure :development do
+  set :debug_assets, true
+  activate :relative_assets
+  require 'rack/middleman/optional_html'
+    use ::Rack::OptionalHtml,
+      root: 'source/',
+      urls: %w[/]
 end
